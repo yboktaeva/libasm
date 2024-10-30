8521b6ee2b0453e7211ce038c35d92648260919f
Yulia BOKTAEVA <yuboktae@made-f0Ar8s10.clusters.42paris.fr>
2024-10-30 18:03:39 +0100

    update

--- ft_strcpy.asm
+++ ft_strcpy.asm
@@ -3,11 +3,13 @@
 global _ft_strcpy
 
 section .text
-
+    ; RDI = dest pointer(d)
+    ; RDI = src pointer(s)
+    ; return dest pointer (d) in RAX
 ft_strcpy:
 
-    xor rax, rax
+    mov rax, rdi    ; store dest pointer in RAX to return later
 
 .loop:
-
+    
 .end:
