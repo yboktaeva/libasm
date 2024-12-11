ee0611159ad21cfb00cc8e3bc17ce4ee00f78527
Yulia BOKTAEVA <yuboktae@paul-f4Br7s6.clusters.42paris.fr>
2024-12-11 18:49:50 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern size_t ft_read(int fd, void *buf, size_t count);
+extern size_t ft_write(int fd, void *buf, size_t count);
+extern int ft_strcmp(const char *s1, const char *s2);
+extern char *ft_strcpy(char *dest, char *src);
+extern char *ft_strdup(const char *s);
+extern size_t ft_strlen(const char *s);
+
+int main() {
+
+}
